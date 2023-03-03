18c3303287f0686f9ab318fcba4156d846ec504f
sheena burch <professional.martinez@gmail.com>
2023-03-02 20:53:24 -0500

    second javascript commit

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let content = document.querySelector(".content");
+let greetingHeader = document.getElementById("greeting-header");
 let timeNow = new Date().getHours();
-console.log (timeNow);
-let greetings = time >= 5 && timeNow < 12 ? "Good Morning" 
-: timeNow >= 12 && timeNow < 18 ? "Good Afternoon"
-: "Good Evening"; 
-console.log (greeting);
-content.innerHTML = `<h1>${greeting}</h1>`
+console.log(timeNow);
+let greeting = timeNow < 12 ? "Good Morning!"
+: timeNow >= 12 && timeNow < 18 ? "Good Afternoon!"
+: "Good Evening!";
+console.log(greeting);
+greetingHeader.innerHTML = `<h1>${greeting}</h1>`;
+
+
+function sendEmail(){
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "professional.martinez@gmail.com",
+    Password : "astlee8898$$",
+    To : 'creatormomstorybook@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : "New Contact Form Inquiry",
+    Body : "Name:" + document.getElementById("name").value
+    + "<br> Email: "+ document.getElementById("email").value
+    + "<br> Message:"+ document.getElementById("message").value
+}).then(
+    message => alert ("Message Sent!")
+);
+}
+
  
