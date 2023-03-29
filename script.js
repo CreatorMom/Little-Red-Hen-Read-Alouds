414e6fe00ee5e143620950b4e263b91fbdc0c6c3
sheena burch <professional.martinez@gmail.com>
2023-03-29 12:10:33 -0400

    eighth JS commit

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+var greet = [
+    'What are you doing that early?',
+    'Good Morning',
+    'Good Afternoon',
+    'Good Evening'
+  ][ parseInt(new Date().getHours() / 24 * 4) ];
+  
+  
+  document.getElementById("greeting").innerHTML = greet;
+  
     let time = new Date();
     time = time.getHours();
     
@@ -33,6 +42,42 @@ Email.send({
     message => alert ("Message Sent!")
 );
 }
+
+
+function validate() {
+    if (document.myForm.name.value == "") {
+        alert("Enter a name");
+        document.myForm.name.focus();
+        return false;
+    }
+    if (!/^[a-zA-Z]*$/g.test(document.myForm.name.value)) {
+        alert("Invalid characters");
+        document.myForm.name.focus();
+        return false;
+    }
+}
+    
+
+
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
+
+
 
 
 
